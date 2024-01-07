c93e8325860493d81016115be0e67370898e88b8
caveat-dev <nathan-jw@users.noreply.github.com>
2024-01-06 22:18:52 -0600

    Initial commit

---
+++ java/org/firstinspires/ftc/teamcode/lib/Alliance.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.lib;
+
+
+/**
+ * For telling AutonomousState what alliance the robot is on.
+ * @see AutonomousState
+ * @author Nathan W
+ */
+public enum Alliance {
+    RED,
+    BLUE,
+}
